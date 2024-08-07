c4d50abf56ce88d0579d2fb9536bab44e0591368
Kristian Jakobsen <kristian@nymann.dev>
2024-08-08 00:27:24 +0200

    Remove other tests

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -1,25 +1,13 @@
 package encode.audio.entrypoint;
 
 import flux.AudioAnnounceTmlg;
-import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.fest.assertions.api.Assertions;
-import org.junit.Assert;
 import org.junit.Test;
 
 @UseReporter(DiffReporter.class)
 public class AudioAnnounceEngineTest {
-
-    @Test
-    public void checkJUnit() {
-        Assert.assertTrue(true);
-    }
-
-    @Test
-    public void checkApprovalTestsReporter() throws Exception {
-        Approvals.verify("My diff reporter open a new window of my default diff tool. It works!");
-    }
 
     @Test
     public void dummyCoverageAudioEngineTest() throws Exception {
