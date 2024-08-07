7efc05e55369bc76e3c6988cc014ee52115e42ac
Kristian Jakobsen <kristian@nymann.dev>
2024-08-08 00:26:38 +0200

    Add Unit tests for coverage

--- src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
+++ src/test/java/encode/audio/entrypoint/AudioAnnounceEngineTest.java
@@ -1,10 +1,11 @@
 package encode.audio.entrypoint;
 
+import flux.AudioAnnounceTmlg;
 import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
+import org.fest.assertions.api.Assertions;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 @UseReporter(DiffReporter.class)
@@ -18,5 +19,20 @@ public class AudioAnnounceEngineTest {
     @Test
     public void checkApprovalTestsReporter() throws Exception {
         Approvals.verify("My diff reporter open a new window of my default diff tool. It works!");
+    }
+
+    @Test
+    public void dummyCoverageAudioEngineTest() throws Exception {
+        // Given
+        AudioAnnounceEngine audioAnnounceEngine = new AudioAnnounceEngine(new LocalHTTPSServer(), new LocalTmpFolder());
+        AudioAnnounceTmlg mp3 = new AudioAnnounceTmlg("http://somehost/10.151.156.180Mon_Nov_04_140724_CET_2013343.mp3", "mp3", "10.151.156.180Mon_Nov_04_140724_CET_2013343.mp3");
+        AudioDataObject audioConfigTmp = new AudioDataObject(".mp3");
+        HttpDataObj httpConfigTmp = new HttpDataObj("./src/test/resources/", "http://localhost/get");
+
+        // When
+        String targetAudioFileMessage = audioAnnounceEngine.publishAudioFile(mp3, audioConfigTmp, httpConfigTmp);
+
+        // Then
+        Assertions.assertThat(targetAudioFileMessage).isNotEmpty();
     }
 }
